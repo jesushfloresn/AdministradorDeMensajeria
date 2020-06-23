df7e2970f218740c6e6a8429e8019145a1200ae5
Joel Dávila <joeldrt@gmail.com>
2020-06-23 18:19:30 -0500

    initial comiit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import NotFound from './pages/404/NotFound';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/dashboard" component={Dashboard} />
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
